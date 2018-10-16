51c9a2147c88e298d051d6421319f3012857584e
Leonardo Sartori <leonardogt4@hotmail.com>
2018-10-16 14:06:21 -0300

    Update GraficoPizzaVaquinhaChurrasco.R
    Alter full code

--- GraficoPizzaVaquinhaChurrasco.R
+++ GraficoPizzaVaquinhaChurrasco.R
@@ -1,15 +1,14 @@
 # Create data for the graph.
-x <- c(15.50, 8.15, 15.15, 10, 5.85)
-labels <- c("Joao", "Maria", "Pedro", "Jose", "Sonia")
+x <- c(400, 250.50, 45.35, 50, 80)
+labels <- c("Food", "Transport", "Water", "Light", "Internet")
 
 # Plot the chart.
-pie(sort(x),labels, main="Vaquinha para o churrasco")
-print("O valor total aquirirido para o churrasco foi:")
+pie(x,labels, main="Expenses")
+print("The total value was:")
 print(sum(x))
-print("O valor médio aquirirido para o churrasco foi:")
+print("The average value was:")
 print(mean(x))
-print("O maior valor de contribuição para o churrasco foi:")
+print("The highest value was:")
 print(max(x))
-print("O menor valor de contribuição para o churrasco foi:")
+print("The lowest value was:")
 print(min(x))
-
