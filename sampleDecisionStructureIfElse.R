021c260ee9c7b00e18ab32c450a60d0d1559f64f
Leonardo Sartori <leonardogt4@hotmail.com>
2018-10-16 18:16:30 -0300

    Add files via upload
    Decision structure (if/else)

---
+++ sampleDecisionStructureIfElse.R
@@ -0,0 +1,9 @@
+#Declare age of people
+agePeople <- as.integer(17)
+
+#Verify if number of age is greater equal the 18
+if(agePeople >= 18){
+  print("Is of legal age!")
+}else{
+  print("Is under age!")
+}
